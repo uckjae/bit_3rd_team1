5652c6c36ebb2e89c4e5953ec47ba8c808fadb88
욱재 <uckjae@naver.com>
2019-12-09 01:18:36 +0900

    vue js

---
+++ NodeJS/lecture_inflearn/wook/app.js
@@ -0,0 +1,19 @@
+console.log("Hello Node!");
+
+var dog = { name: "emy", age:10};
+
+ console.log(dog);
+
+function sum(a,b){
+    return a+ b;
+
+}
+console.log(sum(1,2));
+
+var print = function(){
+    console.log("Hello Node");
+};
+
+print();
+
+setTimeout(print,5000);
---
+++ VueJS/wook/todo/src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+    <button type="button" class="list-group-item text-left" @click="toggleTodoState(todo)" >
+        {{label}}
+    </button>
+</template>
+
+<script>
+    export default {
+        props:['label'],
+        methods:{
+            clickButton(){
+                this.$emit('componentClick');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
