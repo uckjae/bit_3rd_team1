0ff5480606a0bf1f0cb7cfea9de41f417e1c17ad
uckjae.lee <uckjae@naver.com>
2019-12-17 16:49:11 +0900

    node web inflearn 강의

---
+++ NodeJS/nodejs_web_inflearn/wook/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.listen(3000,function(){
+    console.log("start! express on port 3000");
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//url routing
+app.get('/',function(req,res){
+    res.sendFile(__dirname+"/public/main.html");
+});
+
+app.get('/main',function(req,res){
+    res.sendFile(__dirname+"/public/main.html");
+
+});
+
+app.post('/email_post',function(req,res){
+    console.log(req.body.email);
+    res.send("<h1>welcome "+req.body.email+"</h1>");
+});
